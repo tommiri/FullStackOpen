1e8c8548649b6cc8e16e58013b8843e2d3d888ba
Tommi Riiheläinen <90968598+Tommiri@users.noreply.github.com>
2022-12-16 18:16:52 +0200

    Worked on part 4

--- part4/blog/server/app.js
+++ part4/blog/server/app.js
@@ -1,26 +1,26 @@
-const config = require('./utils/config');
-const express = require('express');
+const config = require("./utils/config");
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const blogsRouter = require('./controllers/blogs');
-const { info, error } = require('./utils/logger');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
-info('Connecting to', config.MONGODB_URI);
+logger.info("Connecting to", config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    info('Connected to MongoDB!');
+    logger.info("Connected to MongoDB!");
   })
   .catch((error) => {
-    error('Error connecting to MongoDB:', error.message);
+    logger.error("Error connecting to MongoDB:", error.message);
   });
 
 app.use(cors());
-app.use(express.static('build'));
+app.use(express.static("build"));
 app.use(express.json());
 
-app.use('/api/blogs', blogsRouter);
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
