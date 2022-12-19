121096121b9691b3e384d4da6af1a8acb52da40d
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2022-12-19 15:03:58 +0200

    Few changes

--- part4/notes-example/server/tests/note_api.test.js
+++ part4/notes-example/server/tests/note_api.test.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
-const supertest = require("supertest");
-const app = require("../app");
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
 
 const api = supertest(app);
 
-test("Notes are returned as JSON", async () => {
+test('Notes are returned as JSON', async () => {
   await api
-    .get("/api/notes")
+    .get('/api/notes')
     .expect(200)
-    .expect("Content-Type", /application\/json/);
+    .expect('Content-Type', /application\/json/);
 });
 
 afterAll(() => {
